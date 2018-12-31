add592c47218fef752722254d0db0142eca7890d
Scott Bergler <commandinghands@gmail.com>
2018-12-31 09:40:56 -0800

    add CleanWebpackPlugin requirement to webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Journal with NodeJS',
       template: './src/index.html',
